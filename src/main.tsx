f5f1f02b5c3078bb3cbfb992b4d87d427cdb27e9
Claudia Rodrigues <cacauabreu2013@gmail.com>
2025-07-28 19:04:32 -0300

    fix: Fix styles directory

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "@styles/Main.css";
+import "./assets/styles/Main.css";
 import CustomRouterProvider from "./infrastructure/routes/CustomRouterProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(<CustomRouterProvider />);
